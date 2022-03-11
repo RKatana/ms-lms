5e89ef501b10f287f30d895dd9c45a63f0d86b0a
RKatana <roduor41@gmail.com>
2022-03-11 23:41:51 +0300

    ft-site(add course list view): - add a class to list courses

--- moringaschool/views.py
+++ moringaschool/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from typing import TypeVar as T, QuerySet
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import (CreateView,
+                                       UpdateView,
+                                       DeleteView
+                                      )
 
-# Create your views here.
+from moringaschool.models import Course
+
+class ManageCourseListView(ListView):
+    '''This is the view for course management.
+       The class overrides the get_queryset method of ListView to only retrieve courses created by the current user
+    '''
+    model = Course
+    template_name = '/courses/list.html'
+
+    def get_queryset(self) -> QuerySet[T]:
+        qs = super().get_queryset()
+        return qs.filter(owner= self.request.user)
